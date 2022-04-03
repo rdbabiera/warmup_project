c5205d4bc38035ad02808ca45820d63af53023bc
rdbabiera <rdbabiera@gmail.com>
2022-04-03 12:01:10 -0500

    drive in square code

---
+++ scripts/drive_square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" This script causes Turtlebot to drive in a square """
+import rospy
+from geometry_msgs.msg import Twist
+
+class DriveSquare(object):
+    def __init__(self, duration, linear):
+        rospy.init_node('drive_square')
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=50)
+        rospy.sleep(1)
+
+        # Initialize Speed Properties
+        self.turn_duration = duration
+        self.turn_speed = 1.5708 / self.turn_duration
+        self.linear_speed = linear
+
+    def stop(self):
+        stop = Twist()
+        self.pub.publish(stop)
+
+    def forward(self):
+        forward = Twist()
+        forward.linear.x = self.linear_speed
+        forward.angular.z = 0
+        self.pub.publish(forward)
+
+    def turn(self):
+        turn = Twist()
+        turn.linear.x = 0
+        turn.angular.z = self.turn_speed
+        self.pub.publish(turn)
+
+    def run(self):
+        r = rospy.Rate(5)
+        for i in range(0, 4):
+            # Forward - 6 seconds
+            for j in range(0, 30):
+                self.forward()
+                r.sleep()
+            # Turning - 1 second
+            for j in range(0, self.turn_duration * 5):
+                self.turn()
+                r.sleep()
+        self.stop()
+
+
+if __name__ == '__main__':
+    linear = 0.20
+    turn_duration = 1
+    node = DriveSquare(turn_duration, linear)
+    node.run()
